53ade02286428e180c3d456b7fe335e6472ee6ae
westy12dan <westy12dan@gmail.com>
2023-05-20 15:21:21 +0800

    fix server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
-import { defineConfig } from 'astro/config'
-import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import svelte from '@astrojs/svelte';
 
-import svelte from '@astrojs/svelte'
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        sitemap(),
-        tailwind({
-            config: { applyBaseStyles: false },
-        }),
-        svelte(),
-    ],
-})
+  integrations: [sitemap(), tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), svelte()],
+  output: "server",
+  adapter: vercel()
+});
