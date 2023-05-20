372f60cdd6633b48bf179999aa2ace6e7411ca14
westy12dan <westy12dan@gmail.com>
2023-05-20 12:17:47 +0800

    add svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
-
 import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
 import tailwind from "@astrojs/tailwind";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap(), prefetch(), tailwind()]
+  integrations: [sitemap(), prefetch(), tailwind(), svelte()]
 });
